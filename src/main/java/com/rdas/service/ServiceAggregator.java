eabf45492b38397af5eb72d56b16d186d1644c50
Rana Das <rana.pratap.das@gmail.com>
2018-02-08 07:24:35 +0000

    Aggregator  - wip

--- src/main/java/com/rdas/service/ServiceAggregator.java
+++ src/main/java/com/rdas/service/ServiceAggregator.java
@@ -1,15 +1,47 @@
 package com.rdas.service;
 
+import com.rdas.model.GitHubResponse;
+import com.rdas.model.Item;
 import com.rdas.model.RepositorySummary;
+import com.rdas.model.TwitterResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
 public class ServiceAggregator {
 
-    public RepositorySummary aggregate(){
-        log.info("\n\nWIOOO HOO\n\n");
-        return RepositorySummary.builder().build();
+    private GitHubService gitHubService;
+    private TwitterService twitterService;
+
+    public ServiceAggregator(@Autowired GitHubService gitHubService, @Autowired TwitterService twitterService) {
+        this.gitHubService = gitHubService;
+        this.twitterService = twitterService;
     }
+
+    public List<RepositorySummary> aggregate() throws IOException {
+        GitHubResponse gitHubResponse = gitHubService.search("Reactive");
+        return gitHubResponse.getItems()
+                .stream()
+                .limit(2)
+                .map(item -> itemConverter.apply(item))
+                .collect(Collectors.toList());
+    }
+
+    Function<Item, List<TwitterResponse>> getTweets = item -> twitterService.search(item.getFullName());
+
+    Function<Item, RepositorySummary> itemConverter = item ->
+            RepositorySummary.builder()
+                    .name(item.getName())
+                    .description(item.getDescription())
+                    .publicUrl(item.getHtmlUrl())
+                    .tweets(getTweets.apply(item))
+                    .build();
 }
--- src/test/java/com/rdas/testconfig/SpringTestConfig.java
+++ src/test/java/com/rdas/testconfig/SpringTestConfig.java
@@ -4,7 +4,9 @@ import com.rdas.model.GitHubResponse;
 import com.rdas.service.GitHubService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
+@Profile("test")
 @Configuration
 public class SpringTestConfig {
 
