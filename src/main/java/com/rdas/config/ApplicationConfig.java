f68180567d74eb44667d7bbe06e9feb9a0cd0bab
Rana Das <rana.pratap.das@gmail.com>
2018-02-08 06:05:13 +0000

    Configure Object Mapper

--- src/main/java/com/rdas/config/ApplicationConfig.java
+++ src/main/java/com/rdas/config/ApplicationConfig.java
@@ -1,19 +1,20 @@
 package com.rdas.config;
 
-import com.rdas.model.GitHubResponse;
-import com.rdas.service.GitHubService;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ApplicationConfig {
-    @Bean
-    public GitHubService gitHubService () {
-        return new GitHubService() {
-            @Override
-            public GitHubResponse search(String searchString) {
-                return null;
-            }
-        };
+
+    @Bean(name = "localMapper")
+    public ObjectMapper getMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        mapper.enable(SerializationFeature.INDENT_OUTPUT);
+        return mapper;
     }
 }
